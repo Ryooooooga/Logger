e97f279d1f86e88aaf4ffec98a0357b0e290dc7f
Ryoga <ryoga_314@yahoo.co.jp>
2016-08-08 22:35:35 +0900

    :ok: add Log.hpp

---
+++ Bell/Log.hpp
@@ -0,0 +1,15 @@
+//=====================================================================
+// Copyright (c) 2016 Ryooooooga.
+// https://github.com/Ryooooooga
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//=====================================================================
+
+#pragma once
+
+#include "Log/Log.hpp"
+#include "Log/NullLogger.hpp"
+#include "Log/MultiLogger.hpp"
+#include "Log/FileLogger.hpp"
